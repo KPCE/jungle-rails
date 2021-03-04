c050320a84d8648f8c30ec1ee5a32d6a8dc8bcaf
Jaime Reuss <jamesreuss1@gmail.com>
2021-03-04 02:10:54 +0000

    admin authentication added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"],
+                          password: ENV["HTTP_ADMIN_PASSWORD"]
   def show
   end
 end
